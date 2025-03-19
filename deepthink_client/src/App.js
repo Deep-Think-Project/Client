840cdee7d9998c99beda57d33f34a5a84f5a6d4f
JsHaaaaaan <gkswhdtjr000802@gmail.com>
2025-03-19 22:03:43 +0900

    Server Connection Complete

--- deepthink_client/src/App.js
+++ deepthink_client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
+//서버 킬 때마다 주소 받아와서 package.json에서 proxy 바꿔주기
+
 function App() {
+  const [test, setTest] = useState('');
+  useEffect(() => {
+    axios.get('/main_app/test/',
+      {headers: {
+        "ngrok-skip-browser-warning": "true",  // 🚀 ngrok 경고 페이지 스킵
+      },}
+    )
+      .then(response => {
+        console.log(response.data);                // 객체 전체 콘솔 출력
+        setTest(response.data.message);            // 상태로 message만 저장!
+      })
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <li>{test}</li>
+      <p> hello~ </p>
     </div>
   );
 }
