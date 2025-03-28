4e6b33d2c64987e208fc0dc4a785b12ef24eb9df
김준현 <wnsgus7832@gmail.com>
2025-03-28 20:24:30 +0900

    Fix background color issue on MainPage

--- deepthink_client/src/pages/MainPage.js
+++ deepthink_client/src/pages/MainPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
+import '../styles/MainPage.css'
 
 function MainPage() {
   return (
-    <div>
+    <div className='main-page'>
       <Header /> 
       <SearchBar />
     </div>
