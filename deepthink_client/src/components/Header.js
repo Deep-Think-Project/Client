79914e304cd8920cb9f1b5dc4942158665ed26a0
김준현 <wnsgus7832@gmail.com>
2025-03-29 15:41:37 +0900

    Enable logo click to navigate to the main page

--- deepthink_client/src/components/Header.js
+++ deepthink_client/src/components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from '../assets/logo.png'; 
+import { useNavigate } from 'react-router-dom';
 import '../styles/Header.css';
 
 function Header() {
+  const navigate = useNavigate();
+
+  // 로고 클릭 시 메인 페이지로 이동하는 함수
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
   return (
     <>
       <div className="header-container">
-        <img src={logo} alt="logo" className="logo" />
+        <img src={logo} alt="logo" className="logo" onClick={handleLogoClick}/>
         <div className="service-name">Deep Think</div>
       </div>
       <hr />
